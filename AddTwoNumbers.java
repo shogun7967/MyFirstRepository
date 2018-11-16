da58efaa68f45ee3521ae2c9b40291af3762bed7
shogun7967 <45110026+shogun7967@users.noreply.github.com>
2018-11-17 03:36:22 +0530

    I had created Add Two numbers in Java.

---
+++ AddTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+       int no1,no2;
+       Scanner in;
+       in= new Scanner(System.in);
+       System.out.println("Enter first No....");
+       no1=in.nextInt();
+       System.out.println("Enter Second No....");
+       no2=in.nextInt();
+       System.out.println("Addition is : "+(no1+no2));
+       
+    }
